5e085788e2666e58620632f8985353d2a32ea767
Harish0705 <harish.balachandar@nzme.co.nz>
2024-08-06 15:39:58 +1200

    create mongodb schema

---
+++ model/tasksSchema.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const TaskSchema = mongoose.Schema({
+  taskName: {
+    type: String,
+    required: [true, "Please add a task name"],
+    trim: true, // to remove white space
+    maxLength: [20, "Please enter 20 characters or less"],
+    minLength: [2, "Please enter minimum 2 characters"],
+  },
+  isTaskCompleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export const Task = mongoose.model("Task", TaskSchema);
+
